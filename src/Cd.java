77b8dc39cb4c6b65fa2072689619538fdec02bc0
thibaultcrn1 <tcarron6@outlook.com>
2023-03-14 11:56:48 +0100

    Creéation de la méthode toString

--- src/Cd.java
+++ src/Cd.java
@@ -2,11 +2,15 @@ public class Cd extends Support {
 
     private int nbPistes;
 
-    public Cd(int numero, String titre, double duree, int qttestock, String commentaire, int nbPistes) {
+    public Cd(int numero, String titre, Artiste artiste, double duree, int qttestock, String commentaire, int nbPistes) {
 
-        super(numero, titre, duree, qttestock, commentaire);
+        super(numero, titre, artiste, duree, qttestock, commentaire);
         this.nbPistes = nbPistes;
 
+    }
+
+    public String toString() {
+        return super.toString() + "\nNombre de pistes: " + this.nbPistes;
     }
 
     public int getNbPistes() {
@@ -16,5 +20,5 @@ public class Cd extends Support {
     public void setNbPistes(int nbPistes) {
         this.nbPistes = nbPistes;
     }
-    
+
 }
