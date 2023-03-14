0217b8bdb583256937e3409a2749a2a51f28f0ab
thibaultcrn1 <tcarron6@outlook.com>
2023-03-14 11:58:21 +0100

    Création de la méthode toString, finition du constructeur

--- src/Video.java
+++ src/Video.java
@@ -1,11 +1,22 @@
-import java.util.ArrayList;
-
 public class Video extends Support {
 
-    public Video(int numero, String titre, double duree, int qttestock, String commentaire) {
+    private MetteurScene metteurScene;
+    public Video(int numero, Artiste artiste, String titre, double duree, int qttestock, String commentaire, MetteurScene metteurScene) {
 
-        super(numero, titre, duree, qttestock, commentaire);
+        super(numero, titre, artiste, duree, qttestock, commentaire);
+        this.metteurScene = metteurScene;
 
     }
 
+    public String toString() {
+        return super.toString() + "\nMetteur en scène: " + this.metteurScene.toString();
+    }
+
+    public MetteurScene getMetteurScene() {
+        return this.metteurScene;
+    }
+
+    public void setMetteurScene(MetteurScene metteurScene) {
+        this.metteurScene = metteurScene;
+    }
 }
