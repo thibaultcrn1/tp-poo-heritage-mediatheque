1dd071a2fe7b9cee96b67d4dd6f75d46954448c3
thibaultcrn1 <tcarron6@outlook.com>
2023-03-14 11:24:09 +0100

    Création de la classe Video

---
+++ src/Video.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Video extends Support {
+
+    public Video(int numero, String titre, double duree, int qttestock, String commentaire) {
+
+        super(numero, titre, duree, qttestock, commentaire);
+
+    }
+
+}
