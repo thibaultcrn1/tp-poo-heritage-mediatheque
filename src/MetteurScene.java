08be28995a57ed72b0e048b6bfc3f0c79e473d63
thibaultcrn1 <tcarron6@outlook.com>
2023-03-14 11:14:26 +0100

    Création de la classe MetteurScene et creation du constructor, getter / setter

---
+++ src/MetteurScene.java
@@ -0,0 +1,35 @@
+public class MetteurScene {
+
+    private int identifiant;
+    private String nom;
+    private String prenom;
+
+    public MetteurScene(int identifiant, String nom, String prenom) {
+
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.prenom = prenom;
+
+    }
+
+    public int getIdentifiant() {
+        return this.identifiant;
+    }
+    public String getNom() {
+        return this.nom;
+    }
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+}
